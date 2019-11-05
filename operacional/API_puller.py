45036f0c6db041351487e453377459d3df8e8c73
Eduardo de Moraes <homerico__main__@outlook.com>
2019-11-05 16:49:24 -0300

    Generalizado a forma de puxar conteudo da API e implementado método para criar keys aleatórias

--- operacional/API_puller.py
+++ operacional/API_puller.py
@@ -1,13 +1,26 @@
 # Arquivo para atualizar o conteudo
 import os
+import random
+import string
 import urllib.request as rqst
+
+# Outros arquivos
 import Path_finder as pf
 
-# Para adquirir o caminho do local certo para colocar o .csv:
-path = pf.find('cotacao.csv','/home/homerico/Documentos/POO2/Plotagem')
+# Criar keys aleatórias
+def random_key(key_length=16):
+    letters = string.ascii_uppercase
+    numbers = '0123456789'
+    key_base = letters + numbers
+    return ''.join(random.choice(key_base) for i in range(key_length))
 
-with open(path, 'w') as fin:
-    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' \
-          'MSFT&interval=5min&apikey=YIG0BW9UI8N4X0ZH&datatype=csv'
-    content = rqst.urlopen(url)
-    fin.write(content.read().decode('utf-8'))
+# Para adquirir o caminho do local certo para colocar o .csv:
+def pull(interval='5min',function='TIME_SERIES_INTRADAY'):
+    path = pf.find('cotacao.csv','/home/homerico/Documentos/POO2/Plotagem')
+    url = 'https://www.alphavantage.co/query?function={function}&symbol=MSFT&interval={interval}&apikey={key}&' \
+          'datatype=csv'.format(function=function,interval=interval,key=random_key())
+    #url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=YIG0BW9UI8N4X0ZH&datatype=csv'
+    with open(path, 'w') as fin:
+        content = rqst.urlopen(url)
+        fin.write(content.read().decode('utf-8'))
+        print(content)
