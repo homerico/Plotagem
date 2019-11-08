91a963ea56893a5b3b328c8b2bcbad68af22d179
Eduardo de Moraes <homerico__main__@outlook.com>
2019-11-08 13:49:24 -0300

    Melhorado o script para encontrar o caminho de arquivos e pastas

--- operacional/Path_finder.py
+++ operacional/Path_finder.py
@@ -1,6 +1,12 @@
 import os
 
-def find(name, path):
+def find(name, path, type='file'):
     for root, dirs, files in os.walk(path):
-        if name in files:
-            return os.path.join(root, name)
+        if type=='file':
+            if name in files:
+                return os.path.join(root, name)
+        elif type=='dir':
+            if name in dirs:
+                return os.path.join(root, name)
+
+    raise FileNotFoundError
