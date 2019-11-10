955499be9a40e375ffb63e4a539d3d3e01ff6b5f
Eduardo de Moraes <homerico__main__@outlook.com>
2019-11-10 18:00:56 -0300

    Implementado quatro plotagens de candlesticks e uma métrica comparativa dos valores do 'close'

--- operacional/Chart_plotter.py
+++ operacional/Chart_plotter.py
@@ -8,9 +8,9 @@ from tkinter import *
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 # Modulos para o pandas:
-#from pandas.plotting import register_matplotlib_converters
+from pandas.plotting import register_matplotlib_converters
 import pandas as pd
-#register_matplotlib_converters()
+register_matplotlib_converters()
 
 # Outros scripts:
 import Path_finder as pf
@@ -26,53 +26,65 @@ class App(Tk):
         data = pd.read_csv(path)
         data.loc[:, 'timestamp'] = data.loc[:, 'timestamp'].map(mdates.datestr2num)
         data = data.loc[:,['timestamp','open','high','low','close']]
+        self.data = data
 
-        # SMA:
+        # Simple moving average(SMA):
         name = ap.pull(function='SMA',interval='daily',series_type='close',time_period='15')
         path = pf.find('{}.csv'.format(name), '/home/homerico/Documentos/POO2/Plotagem')
         data_sma = pd.read_csv(path)
         data_sma = data_sma.loc[0:100,['time','SMA']]
         data_sma.loc[:, 'time'] = data_sma.loc[:, 'time'].map(mdates.datestr2num)
+        self.data_sma = data_sma
 
+        # Exponential moving average(EMA):
+        name = ap.pull(function='EMA', interval='daily', series_type='close', time_period='15')
+        path = pf.find('{}.csv'.format(name), '/home/homerico/Documentos/POO2/Plotagem')
+        data_ema = pd.read_csv(path)
+        data_ema = data_ema.loc[0:100, ['time', 'EMA']]
+        data_ema.loc[:, 'time'] = data_ema.loc[:, 'time'].map(mdates.datestr2num)
+        self.data_ema = data_ema
+
+        # Double exponential moving average(DEMA):
+        name = ap.pull(function='DEMA', interval='daily', series_type='close',time_period='15')
+        path = pf.find('{}.csv'.format(name), '/home/homerico/Documentos/POO2/Plotagem')
+        data_macd = pd.read_csv(path)
+        data_macd = data_macd.loc[0:100, ['time', 'DEMA']]
+        data_macd.loc[:, 'time'] = data_macd.loc[:, 'time'].map(mdates.datestr2num)
+        self.data_macd = data_macd
+
+    def get_canvas(self,data_candle,data_metric=None,name_metric='',num=1):
         ax = plt.subplot()
-        fig = plt.subplot()
-        fig.plot(data_sma['time'], data_sma['SMA'], color='green', label='SMA5')
-        candlestick_ohlc(ax, data.values, width=0.7, colorup='g', colordown='r')
+        if name_metric != '':
+            fig = plt.subplot()
+            fig.plot(data_metric['time'], data_metric[name_metric], color='green', label=name_metric)
+        candlestick_ohlc(ax, data_candle.values, width=0.7, colorup='g', colordown='r')
         #ax.grid(True)
         ax.set_axisbelow(True)
-        ax.set_title('SMA', color='white')
+        ax.set_title(name_metric, color='white')
         ax.set_facecolor('black')
         ax.figure.set_facecolor('#121212')
         ax.tick_params(axis='x', colors='white')
         ax.tick_params(axis='y', colors='white')
         ax.xaxis_date()
         canvas = FigureCanvasTkAgg(ax.figure, self)
-        canvas.get_tk_widget().grid(row=0,column=0)
+        del ax
+        return canvas
+        #canvas.get_tk_widget().grid(row=0,column=0)
         #canvas.get_tk_widget().pack(side=LEFT, fill=BOTH, expand=False)
-        canvas2 = FigureCanvasTkAgg(fig.figure, self)
-        canvas2.get_tk_widget().grid(row=0, column=1)
+        #canvas2 = FigureCanvasTkAgg(fig.figure, self)
+        #canvas2.get_tk_widget().grid(row=1, column=0)
         #canvas2.get_tk_widget().pack(side=RIGHT, fill=BOTH, expand=False)
-
-
-        """fig = Figure(figsize = (7.5, 4.5), dpi = 100)
-        ax = fig.add_subplot(111)
-
-
-        ax.xaxis.set_major_locator(mondays)
-        ax.xaxis.set_minor_locator(alldays)
-        ax.xaxis.set_major_formatter(weekFormatter)
-        candlestick_ohlc(ax, data, width=0.6)
-        ax.xaxis_date()
-        ax.autoscale_view()
-        plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
-
-        canvas = FigureCanvasTkAgg(fig, self)
-        canvas.show()
-        canvas.get_tk_widget().pack(side = TOP,  fill = BOTH,  expand = False)"""
-
 
 if __name__ == "__main__":
     app = App()
-    app.geometry("1920x1080+51+51")
+    canvas_candle = app.get_canvas(data_candle=app.data)
+    canvas_candle.get_tk_widget().grid(row=0,column=0)
+    canvas_sma = app.get_canvas(data_candle=app.data,data_metric=app.data_sma,name_metric='SMA',num=2)
+    canvas_sma.get_tk_widget().grid(row=0, column=1)
+    canvas_ema = app.get_canvas(data_candle=app.data,data_metric=app.data_ema,name_metric='EMA',num=3)
+    canvas_ema.get_tk_widget().grid(row=1, column=0)
+    canvas_macd = app.get_canvas(data_candle=app.data,data_metric=app.data_macd,name_metric='DEMA',num=4)
+    canvas_macd.get_tk_widget().grid(row=1, column=1)
+    app.geometry("1000x800")
     app.title("Candlestick")
     app.mainloop()
